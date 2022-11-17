c61940b0270177a379430c50c4e651f77d8e1893
Timofey <xalvaine@gmail.com>
2022-11-17 20:02:20 +0300

    [feat] Add collection gradient field (#49)
    * [feat] Add collection gradient field
    
    * [feat] Update tests

--- src/collections/dto/create-collection.dto.ts
+++ src/collections/dto/create-collection.dto.ts
@@ -1,11 +1,48 @@
 import {
   IsEnum,
   IsNotEmpty,
+  IsNumber,
   IsOptional,
   IsString,
   Length,
+  Max,
+  Min,
+  ValidateNested,
 } from 'class-validator'
 import { CollectionType } from '../entities/collection.entity'
+import { Type } from 'class-transformer'
+
+class Color {
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  @Max(255)
+  red: number
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  @Max(255)
+  green: number
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  @Max(255)
+  blue: number
+}
+
+class CollectionGradient {
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => Color)
+  startColor: Color
+
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => Color)
+  endColor: Color
+}
 
 export class CreateCollectionDto {
   @IsNotEmpty()
@@ -20,4 +57,9 @@ export class CreateCollectionDto {
   @IsString()
   @Length(0, 1024)
   description?: string
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => CollectionGradient)
+  gradient?: CollectionGradient
 }
