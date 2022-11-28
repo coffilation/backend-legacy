a410a70174ccec2118e169bcfd9e9cb8521bb762
xalvaine <xalvaine@gmail.com>
2022-11-28 13:24:12 +0300

    [fix] Throw exception for request with invalid tokens and optional auth

--- src/auth/guards/unsafe-extract-user-jwt-auth.guard.ts
+++ src/auth/guards/unsafe-extract-user-jwt-auth.guard.ts
@@ -4,10 +4,11 @@ import { JwtAuthGuard } from './jwt-auth.guard'
 @Injectable()
 export class UnsafeExtractUserJwtAuthGuard extends JwtAuthGuard {
   async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest()
     try {
       await super.canActivate(context)
     } catch (error) {
-      if (error.status !== 401) {
+      if (error.status !== 401 || request.headers.authorization !== undefined) {
         throw error
       }
     }
